0f0338d224065696ad88ed2fed7af47e3635e8f2
Michael Favre <favre_michael@yahoo.fr>
2023-09-10 16:19:06 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Michael Favre on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
